d846b0d5bdf0c7cd86de408a92fbeb529ded4298
Sarasi <sarasimadahasi@gmail.com>
2025-08-08 14:51:53 +0530

    test

--- frontend/src/components/Whyadopt/Whyadopt.jsx
+++ frontend/src/components/Whyadopt/Whyadopt.jsx
@@ -1,1 +1,17 @@
 import React from "react";
+
+const Whyadopt = () => {
+    return (
+        <div className="why-adopt">
+        <h2>Why Adopt?</h2>
+        <p>Adopting a pet can bring joy and companionship to your life.</p>
+        <ul>
+            <li>Save a life</li>
+            <li>Gain a loyal friend</li>
+            <li>Support your local community</li>
+        </ul>
+        </div>
+    );
+    }
+
+export default Whyadopt;
